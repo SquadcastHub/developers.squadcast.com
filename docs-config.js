b907ec894f2a13bd6237649f6eeb107de3884942
ShubhanjanMedhi-dev <shubhanjan@squadcast.com>
2022-01-25 19:28:22 +0530

    initial commit

---
+++ docs-config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  product: "Squadcast Developers",
+  pathPrefix: "/",
+  productIconKey: "",
+  contentRepo: "squadcasthub",
+  contentRepoFolder: "developers.squadcast.com",
+  externalLinks: [
+    {
+      title: "Squadcast on GitHub",
+      url: "https://github.com/squadcasthub"
+    },
+    {
+      title: "Squadcast documentation",
+      url: "https://support.squadcast.com"
+    },
+  ],
+  search: {
+    indexName: "",
+    apiKey: "",
+    algoliaOptions: { "facetFilters": "" }
+  },
+  siteMetadata: {
+    title: "Squadcast Developers",
+    description: "Squadcast Developer Portal",
+    author: "@squadcast",
+    url: "https://squadcast.com",
+    image: "",
+  }
+}
